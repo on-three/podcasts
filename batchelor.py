e56a3d1baf8f889695606add02314dbe62fc567f
on-three <on.three.email@gmail.com>
2018-02-18 12:38:21 -0800

    Added force reconstruct and guard against redownloads. cleanup

--- batchelor.py
+++ batchelor.py
@@ -15,7 +15,7 @@ SHOW_NAME='the.John.Batchelor.Show'
 
 def download_segment(name, url, title, trim=False, intro=True, force=False):
   # does the file already exist? if so don't download again
-  if os.path.exists(name):
+  if os.path.exists(name) and not force:
     return True
 
   call = 'wget --tries=5 --user-agent="Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0" -O "{name}" "{url}"'.format(name=name, url=url)
@@ -29,7 +29,6 @@ def download_segment(name, url, title, trim=False, intro=True, force=False):
     raw_cmd = 'mv {n} {r}'.format(n=name, r=raw_name)
     os.system(raw_cmd)
     c = 'ffmpeg -i {raw_name} -y -ss {t} -vcodec copy -acodec copy {name}'.format(t=60, raw_name=raw_name, name=name)
-    print("*****" + c + "*******")
     os.system(c)
 
   if intro:
@@ -64,16 +63,15 @@ def aggregate_files(outfile, tmp_files, force=False):
   call = 'ffmpeg -y -f concat -safe 0 -i {f} -c copy {outfile}'.format(f=listfile, outfile=outfile)
   os.system(call)
 
-def reassemble_program(rss, trim=False, out_dir='.', tmp_dir='/tmp'):
+def reassemble_program(rss, trim=False, force=False, out_dir='.', tmp_dir='/tmp'):
   """
   """
   print 'Aggregating episode at rss: ' + rss
   feed = feedparser.parse( rss )
   print feed[ "channel" ][ "title" ]
   print feed[ "channel" ][ "description" ]
-
-  #print feed[ 'channel' ]['item'][ 'media_content']
-
+  
+  # DEBUG
   #print str(feed)
 
   # sort channel entries, most recent first
@@ -88,8 +86,15 @@ def reassemble_program(rss, trim=False, out_dir='.', tmp_dir='/tmp'):
 
   if len(newest_entries) < 1:
     print("**** couldn't find any entries in rss feed.")
-    return
+    return False
 
+  # don't regenerate the target file unless forced
+  basename = SHOW_NAME + '.' + str(today.tm_mon) + '.' + str(today.tm_mday) + '.' + str(today.tm_year)
+  outfile = out_dir + '/' + basename + '.mp3'
+  if os.path.exists(outfile) and not force:
+    print "Target file: " + outfile + " already exists. Use --force to reconstruct."
+    return False
+ 
   files = []
   for entry in sorted_entries:
     date = entry['published_parsed']
@@ -101,26 +106,18 @@ def reassemble_program(rss, trim=False, out_dir='.', tmp_dir='/tmp'):
     
   i = 0
   tmp_files = []
-  basename = SHOW_NAME + '.' + str(today.tm_mon) + '.' + str(today.tm_mday) + '.' + str(today.tm_year)
   for f in files:
     date = f['published_parsed']
-    print str(i) + '+++++++++++'
     title = f['title']
-    print title
-    
-    print str(date)
     mp3 = f['media_content'][0]['url']
     #img = entry['media_content'][1]['url']
-    print 'mp3 ' + mp3
-    print '++++++++++++'
     i = i + 1
 
     # download the segment
     tmp_file = tmp_dir + '/' + basename + '.' + str(i) + '.mp3'
-    download_segment(tmp_file, mp3, title, trim=trim)
+    download_segment(tmp_file, mp3, title, trim=trim, force=force)
     tmp_files.append(tmp_file)
 
-  outfile = out_dir + '/' + basename + '.mp3' 
 
   aggregate_files(outfile, tmp_files)
 
@@ -134,22 +131,16 @@ def main():
   parser = argparse.ArgumentParser(description='Reassemble John Batchelor episode from RSS feed.')
   parser.add_argument('--rss', action="store", default="https://audioboom.com/channels/4002274.rss")
   parser.add_argument('-t', '--trim', action="store_true", default=True, help='trim one minute from front end of all segments.')
-  #parser.add_argument('--phonemes', action="store_true", default=False)
-  #parser.add_argument('--videos', action="store_true", default=False)
+  parser.add_argument('--force', action="store_true", default=False, help='Force redownload and reconstruction of entire file.')
   parser.add_argument('-o', '--outdir', type=str, default='./tmp/')
   args = parser.parse_args()
 
-  #print(" in dir: {dir}".format(dir=args.outdir))
 
   rss = args.rss
   trim = args.trim
+  force = args.force
 
-  #infile = args.infile
-  #if not os.path.isfile(infile):
-  #  print("File: " + infile + " not found.")
-  #  return -1
-
-  reassemble_program(rss, trim=trim)
+  reassemble_program(rss, trim=trim, force=force)
 
 
 
