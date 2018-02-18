0336344b71ff7535b798f45eb0fa989a39efd060
on-three <on.three.email@gmail.com>
2018-02-18 13:37:29 -0800

    Added support to download by date

--- batchelor.py
+++ batchelor.py
@@ -13,12 +13,24 @@ from naturalreaders import do_tts
 SHOW_NAME='the.John.Batchelor.Show'
 
 
+class SimpleDate(object):
+  def __init__(self, date_string):
+    d = date_string.split(':')
+    self.tm_mon = int(d[0])
+    self.tm_mday = int(d[1])
+    self.tm_year = int(d[2])
+    
+
 def download_segment(name, url, title, trim=False, intro=True, force=False):
   # does the file already exist? if so don't download again
   if os.path.exists(name) and not force:
     return True
 
+  print("File " + name + " does not exist.")
+  print("Downloading sement of show at url: " + url)
+
   call = 'wget --tries=5 --user-agent="Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0" -O "{name}" "{url}"'.format(name=name, url=url)
+  print "****" + call + "****"
   os.system(call)
 
   if not os.path.exists(name):
@@ -63,7 +75,7 @@ def aggregate_files(outfile, tmp_files, force=False):
   call = 'ffmpeg -y -f concat -safe 0 -i {f} -c copy {outfile}'.format(f=listfile, outfile=outfile)
   os.system(call)
 
-def reassemble_program(rss, trim=False, force=False, out_dir='.', tmp_dir='/tmp'):
+def reassemble_program(rss, trim=False, force=False, out_dir='./', tmp_dir='/tmp', date=None):
   """
   """
   print 'Aggregating episode at rss: ' + rss
@@ -80,9 +92,11 @@ def reassemble_program(rss, trim=False, force=False, out_dir='.', tmp_dir='/tmp'
   newest_entries = sorted(entries, key=lambda entry: entry["published_parsed"])
   newest_entries.reverse() # for most recent entries first
 
-  #only take entries from the latest episode
+  #only take entries from the latest episode if no date provided
   today = newest_entries[0]['published_parsed']
-  print 'Aggregating segments for show from ' + str(today)
+  if date:
+    today = SimpleDate(date)
+  print 'Aggregating segments for show from ' + str(today.tm_mon) + ':' + str(today.tm_mday) + ':' + str(today.tm_year)
 
   if len(newest_entries) < 1:
     print("**** couldn't find any entries in rss feed.")
@@ -97,17 +111,20 @@ def reassemble_program(rss, trim=False, force=False, out_dir='.', tmp_dir='/tmp'
  
   files = []
   for entry in sorted_entries:
-    date = entry['published_parsed']
+    _date = entry['published_parsed']
     #tm_year=2018, tm_mon=2, tm_mday=17
-    if date.tm_year != today.tm_year or date.tm_mon != today.tm_mon or date.tm_mday != today.tm_mday:
+    if _date.tm_year != today.tm_year or _date.tm_mon != today.tm_mon or _date.tm_mday != today.tm_mday:
       continue;
   
     files.append(entry)
+
+  if len(files) == 0:
+    print("Could not find any rss entries for indicated date. Bailing.")
+    return
     
   i = 0
   tmp_files = []
   for f in files:
-    date = f['published_parsed']
     title = f['title']
     mp3 = f['media_content'][0]['url']
     #img = entry['media_content'][1]['url']
@@ -115,7 +132,9 @@ def reassemble_program(rss, trim=False, force=False, out_dir='.', tmp_dir='/tmp'
 
     # download the segment
     tmp_file = tmp_dir + '/' + basename + '.' + str(i) + '.mp3'
-    download_segment(tmp_file, mp3, title, trim=trim, force=force)
+    if not download_segment(tmp_file, mp3, title, trim=trim, force=force):
+      print("Downloading segment " + tmp_file + " at url: " + mp3 + " failed for some reason. Bailing.")
+      return False
     tmp_files.append(tmp_file)
 
 
@@ -132,15 +151,28 @@ def main():
   parser.add_argument('--rss', action="store", default="https://audioboom.com/channels/4002274.rss")
   parser.add_argument('-t', '--trim', action="store_true", default=True, help='trim one minute from front end of all segments.')
   parser.add_argument('--force', action="store_true", default=False, help='Force redownload and reconstruction of entire file.')
-  parser.add_argument('-o', '--outdir', type=str, default='./tmp/')
+  parser.add_argument('-o', '--outdir', type=str, default='./', help='Directory to put final reconstructed file.')
+  parser.add_argument('--tmpdir', type=str, default='/tmp', help='Directory used for temporary work files.')
+  parser.add_argument('--date', type=str, default=None, help='Reconstruct a show on a specific date in the form "month:day:fullyear" e.g. "2:17:2018"')
   args = parser.parse_args()
 
 
   rss = args.rss
   trim = args.trim
   force = args.force
+  out_dir = args.outdir
+  tmp_dir = args.tmpdir
+  date = args.date
+  if date:
+    # check our date is of the correct form
+    date_regex = r'[0-9]{1,2}:[0-9]{1,2}:[0-9]{4}'
+    if not re.match(date_regex, date):
+      print 'Provided date argument ' + date + ' is not correct. Use form "2:21:2018"'
+      return
+    else:
+      print 'Will attempt to reconstruct show for date: ' + date
 
-  reassemble_program(rss, trim=trim, force=force)
+  reassemble_program(rss, out_dir=out_dir, tmp_dir=tmp_dir, trim=trim, force=force, date=date)
 
 
 
