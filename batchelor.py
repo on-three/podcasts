5446f0b798301ee1402a58d3756de5295cef572f
on-three <on.three.email@gmail.com>
2018-02-28 23:14:57 -0800

    Adjustable commercial trimming

--- batchelor.py
+++ batchelor.py
@@ -25,7 +25,7 @@ def get_runnning_time_seconds(filename):
   return int(audio.info.length)
 
 
-def download_segment(name, url, title, trim=False, intro=True, force=False):
+def download_segment(name, url, title, trim=43, intro=True, force=False):
   # does the file already exist? if so don't download again
   if os.path.exists(name) and not force:
     return True
@@ -40,8 +40,8 @@ def download_segment(name, url, title, trim=False, intro=True, force=False):
   if not os.path.exists(name):
     return False
 
-  if trim:
-    COMMERCIAL_LENGTH = 60
+  if trim > 0:
+    COMMERCIAL_LENGTH = trim
     # we want to trim a second from the start and end of the file
     # but to do that ffmpeg needs the entire running time (piece of shit)
     length_s = get_runnning_time_seconds(name)
@@ -88,7 +88,7 @@ def aggregate_files(outfile, tmp_files, force=False):
   call = 'ffmpeg -y -f concat -safe 0 -i {f} -c copy {outfile}'.format(f=listfile, outfile=outfile)
   os.system(call)
 
-def reassemble_program(rss, trim=False, force=False, out_dir='./', tmp_dir='/tmp', date=None):
+def reassemble_program(rss, trim=43, force=False, out_dir='./', tmp_dir='/tmp', date=None):
   """
   """
   print 'Aggregating episode at rss: ' + rss
@@ -163,7 +163,7 @@ def reassemble_program(rss, trim=False, force=False, out_dir='./', tmp_dir='/tmp
 def main():
   parser = argparse.ArgumentParser(description='Reassemble John Batchelor episode from RSS feed.')
   parser.add_argument('--rss', action="store", default="https://audioboom.com/channels/4002274.rss")
-  parser.add_argument('-t', '--trim', action="store_true", default=True, help='trim one minute from front end of all segments.')
+  parser.add_argument('-t', '--trim', type=int, default=43, help='trim n seconds from the front end back of each segment.')
   parser.add_argument('--force', action="store_true", default=False, help='Force redownload and reconstruction of entire file.')
   parser.add_argument('-o', '--outdir', type=str, default='./', help='Directory to put final reconstructed file.')
   parser.add_argument('--tmpdir', type=str, default='/tmp', help='Directory used for temporary work files.')
